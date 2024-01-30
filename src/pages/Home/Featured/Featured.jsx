abe8f6f832057448cf03837739cfd0986edeb7ab
Romel180149 <mosiurrahmanromel180149@gmail.com>
2024-01-30 22:49:03 +0600

    testimonial added

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -10,7 +10,7 @@ const Featured = () => {
       <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle  subHeading="check it out" heading="Featured item" ></SectionTitle>
         
-        <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-40 pb-20 pt-12 px-36">
+        <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
             <div>
               <img src={featuredImg} alt="" />  
             </div>
@@ -18,7 +18,7 @@ const Featured = () => {
                 <p> Aug 20,2029</p>
                 <p className="uppercase">Where can i get some?</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eos vitae libero inventore eaque maiores officiis. Ducimus maxime eos nihil molestiae! Dicta dignissimos vero et perferendis beatae ipsam natus, ullam tenetur quasi tempore dolores omnis minima repellat nesciunt necessitatibus quos sequi assumenda tempora autem, quia odio. Natus non dicta necessitatibus.</p>
-                <button className="btn btn-outline">Order now</button>
+                <button className="btn btn-outline border-0 border-b-4 mt-4">Order now</button>
             </div>
         </div>
         </div>
