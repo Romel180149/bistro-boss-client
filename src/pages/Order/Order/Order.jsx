624bb34519616a954827167d7b777011c7b23e19
Romel180149 <mosiurrahmanromel180149@gmail.com>
2024-02-07 10:57:33 +0600

    order item added

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,10 +1,46 @@
 
-import orderCoverImg from '../../../assets/shop/order.jpg.jpg'
+import { useState } from 'react';
+import orderCoverImg from '../../../assets/shop/order.jpg'
 import Cover from '../../Shared/Cover/Cover';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../../Hooks/useMenu';
+import FoodCard from '../../../components/FoodCard/FoodCard';
 const Order = () => {
+    const [tabIndex,setTabIndex] = useState(0);
+    const [menu] =useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
-           <Cover img={orderCoverImg}   title="Order Food"   >   </Cover> 
+           <Cover img={orderCoverImg}   title="Order Food"   >   </Cover>
+           <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+  <TabList>
+    <Tab>SALAD</Tab>
+    <Tab>PIZZA</Tab>
+    <Tab>SOUP</Tab>
+    <Tab>DESSERT</Tab>
+    <Tab>DRINKS</Tab>
+  </TabList>
+ 
+  <TabPanel>
+    {
+      salad.map(item=><FoodCard
+        key={item._id}
+        item={item}
+        ></FoodCard>)
+    }
+
+  </TabPanel>
+  <TabPanel></TabPanel>
+  <TabPanel></TabPanel>
+  <TabPanel></TabPanel>
+  <TabPanel></TabPanel>
+</Tabs> 
+   
         </div>
     );
 };
